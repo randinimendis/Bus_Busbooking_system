b6039eef3fe8cb844f12d26308b1a236ffe110b4
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-08-28 19:32:57 +0530

    dev_10_commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var albox = document.getElementById("alert-additional-content-5");
+var close = document.getElementById("close");
+
+function Close(){
+    albox.style.opacity = 0;
+    albox.style.transition = "all 3s";
+}
