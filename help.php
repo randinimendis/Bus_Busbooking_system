0eb185b12eecf69aadf02363b990577da13e6e47
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-08-29 15:18:37 +0530

    dev_15_commit

---
+++ help.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HELP PAGE</title>
+    <link rel="stylesheet" href="cssfile/help.css"/>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+   <style type="text/css">
+       body{
+        background-image: url(image/4.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
+       }
+   </style>
+  
+</head>
+<body>
+  
+
+ 
+            <!-- body part my entervcode -->
+
+      <div class="wrapper">
+        <h2>HELP CENTER</h2>
+      <div class="line"><br><br><br><br>
+      </div>
+      </div>
+
+      <div class="single-service">
+            <div class="help">
+         <i class="fa fa-search"></i></div>
+        <span></span>
+             <h3><b>Customer Support<b></h3><br><br>
+      <p>
+
+        We help you to make your<br> journey better.</p>
+      </div>
+
+
+      <div class="single-service">
+            <div class="help">
+      <i class="fa fa-headphones"></i></div>
+      <span></span>
+             <h3><b>More Choices<b></h3><br><br>
+      <p>
+
+        We give you maximum choices across<br> all the routes to choose your bus.</p>
+      </div>
+
+
+      
+      <div class="single-service">
+        <div class="help">
+  <i class="fa fa-map-marker"></i></div>
+  <span></span>
+         <h3><b>Google Map Location<b></h3><br><br>
+  <p>
+    Google Map Location
+
+    We send you the boarding place and destination<br> place link in google map.
+     </p>
+  </div>
+
+  
+ 
+<script src="js/mystyle.js"></script>
+
+
+</body>
+</html>
+
+
+
