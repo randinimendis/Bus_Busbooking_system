b1119f8345aca0642973b1593a9a607e29e127a0
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-08-29 15:18:27 +0530

    dev_14_commit

---
+++ function.php
@@ -0,0 +1,52 @@
+<?php
+
+function check_login($conn)
+{
+
+	if(isset($_SESSION['user_id']))
+	{
+
+		$id = $_SESSION['user_id'];
+		$query = "select * from users where user_id = '$id' limit 1";
+
+		$result = mysqli_query($conn,$query);
+		if($result && mysqli_num_rows($result) > 0)
+		{
+
+			$user_data = mysqli_fetch_assoc($result);
+			return $user_data;
+		}
+	}
+
+	//redirect to login
+	header("Location: Login.php");
+	die;
+
+}
+
+
+function random_num($length)
+{
+
+	$text = "";
+	if($length < 5)
+	{
+		$length = 5;
+	}
+
+	$len = rand(4,$length);
+
+	for ($i=0; $i < $len; $i++) { 
+		# code...
+
+		$text .= rand(0,9);
+	}
+
+	return $text;
+}
+
+
+
+
+
+?>
